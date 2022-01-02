06b55ccb4219268bdc1e9773d1986145c9486038
Serhat <serhatataman13@hotmail.com>
2022-01-02 02:13:47 +0100

    refactoring

--- image_to_2d_array.py
+++ image_to_2d_array.py
@@ -1,11 +1,12 @@
-import PIL as pl
+import PIL
 from skimage.io import imread
 import numpy as np
 
 
 # https://stackoverflow.com/questions/27026866/convert-an-image-to-2d-array-in-python
-# im = imread("avatar.png")
-# indices = np.dstack(np.indices(im.shape[:2]))
-# data = np.concatenate((im, indices), axis=-1)
-# print(data)
+im = imread("avatar.png")
+indices = np.dstack(np.indices(im.shape[:2]))
+data = np.concatenate((im, indices), axis=-1)
+print(data)
+
 
