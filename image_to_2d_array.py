a1738e32335e59234e45432e82e856b7c45ca57a
Serhat <serhatataman13@hotmail.com>
2022-01-04 23:41:52 +0100

    image to pixel art

--- image_to_2d_array.py
+++ image_to_2d_array.py
@@ -4,9 +4,13 @@ import numpy as np
 
 
 # https://stackoverflow.com/questions/27026866/convert-an-image-to-2d-array-in-python
-im = imread("avatar.png")
+im = imread("snowboarder.jpg")
 indices = np.dstack(np.indices(im.shape[:2]))
 data = np.concatenate((im, indices), axis=-1)
-print(data)
+new_data = data[:, :, :]
+print(new_data)
+
+# np.savetxt("somefile.txt", new_data.reshape((4,5,10)), newline="\n")
+
 
 
